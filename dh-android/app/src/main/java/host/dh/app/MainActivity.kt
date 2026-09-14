8665a22644ceeeb80ef867970792920d6c8ba429
Mrhotcafe <86748736+Mrhotcafe@users.noreply.github.com>
2026-09-13 21:27:00 -0500

    chore(dh): scaffold: dh-android/app/src/main/java/host/dh/app/MainActivity.kt

---
+++ dh-android/app/src/main/java/host/dh/app/MainActivity.kt
@@ -0,0 +1,13 @@
+package host.dh.app
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    findViewById<TextView>(R.id.headline).text = getString(R.string.headline)
+  }
+}
